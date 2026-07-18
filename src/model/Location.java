7b6f462ec846ee74a8ca24841a091bb1d83c7d77
Sithumini Anuhansi <anuhansisithumini@gmail.com>
2026-07-18 17:12:21 +0530

    Graph Feature: Add Location Model

---
+++ src/model/Location.java
@@ -0,0 +1,61 @@
+package model;
+
+import java.util.Objects;
+
+public class Location {
+
+    private String locationId;
+    private String locationName;
+    private double latitude;
+    private double longitude;
+
+    public Location(String locationId, String locationName,
+                    double latitude, double longitude) {
+
+        this.locationId = locationId;
+        this.locationName = locationName;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getLocationId() {
+        return locationId;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    @Override
+    public String toString() {
+        return locationName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o)
+            return true;
+
+        if (!(o instanceof Location))
+            return false;
+
+        Location location = (Location) o;
+
+        return Objects.equals(locationId, location.locationId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(locationId);
+    }
+
+}
